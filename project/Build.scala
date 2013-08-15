f38a3dfeb4610babd0bcf03d4d5715596ebe0cd5
mart07 <rodrigo.martinez@springer.com>
2013-08-15 15:17:40 +0200

    gitignore and scalatest lib extended

--- project/Build.scala
+++ project/Build.scala
@@ -9,6 +9,7 @@ object ApplicationBuild extends Build {
 
     val appDependencies = Seq(
       // Add your project dependencies here,
+      "org.scalatest" %% "scalatest" % "1.9.1" % "test"
     )
 
     val main = play.Project(appName, appVersion, appDependencies).settings(
